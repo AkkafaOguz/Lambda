5bd19ab1b5629a10cd273073ff05d3474e5304ad
Oguzhan <akkafaoguz@gmail.com>
2021-09-14 11:41:10 +0300

    Questions

---
+++ src/Questions.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Questions {
+
+    public static void main(String[] args) {
+
+        List<Integer> l = new ArrayList<>();
+
+        l.add(12);
+        l.add(9);
+        l.add(131);
+        l.add(14);
+        l.add(9);
+        l.add(10);
+        l.add(4);
+        l.add(12);
+        l.add(15);
+
+        //1) Print the list element on the console in the same line with a spoace between two consecutive elements.
+        //2) Print the even list elements on the console in the same line with a spoace between two consecutive elements.
+        //3) Print the square of odd  list elements on the console in the same line with a spoace between two consecutive elements.
+        //4) Print the cube if distinct odd list elements on the console in the same line with a spoace between two consecutive elements.
+        //5) Print the calculation of the sum of squares of distinct even elements
+        //6) Print the calculation of the product of the cubes of distinct even elements
+        //7) Print the maximum value from the list elements
+        //8) Print the minimum value from the list elements
+        //9) Find the minimum value which is greater than seven and even
+        //10) Find the half of the elements which are distinct and greater than 5 in reverse order from the list.
+
+
+
+        List<Integer> l2 = new ArrayList<>();
+
+        l2.add(12);
+        l2.add(9);
+        l2.add(13);
+        l2.add(4);
+        l2.add(9);
+        l2.add(2);
+        l2.add(4);
+        l2.add(12);
+        l2.add(15);
+
+        //1) Create a method to print the list elements on the console in the same line with a space between two consecutive elements.(Method Reference)
+        //2) Create a method to print the even list elements on the console in the same line with a space between two consecutive elements.(Method Reference)
+        //3)Create a method to print the square of odd list elements on the console in the same line with a space between two consecutive elements.
+        //4)Create a method to print the cube of distinct odd list elements on the console in the same line with a space between two consecutive elements.
+        //5)Create a method to calculate the sum of the squares of distinct even elements
+        //6)Create a method to calculate the product of the cubes of distinct even elements
+        //7)Create a method to find the maximum value from the list elements
+        //8)Create a method to find the minimum value from the list elements
+        //9)Create a method to find the minimum value which is greater than 7 and even from the list
+        //10) Find the half of the elements which are distinct and greater than 5 in reverse order from the list.
+
+
+        List<String> l3 = new ArrayList<>();
+
+        l3.add("Ali");
+        l3.add("Ali");
+        l3.add("Mark");
+        l3.add("Amanda");
+        l3.add("Christopher");
+        l3.add("Jackson");
+        l3.add("Mariano");
+        l3.add("Alberto");
+        l3.add("Tucker");
+        l3.add("Benjamin");
+
+        //1) Create a method to print all list elements in uppercase
+        //2) Create a method to print the elements after ordering according to their lengths
+        //3) Create a method to sort the distinct elements by using their last characters
+        //4) Create a method to sort the elements according to their lengths then according to their first character
+        //5) Remove the elements if the length of the element is greater than 5
+        //6) Remove the elements if the element is starting with 'A', 'a' or ending with 'N', 'n'
+        //7) Create a method which takes the square of the length of every element, prints them distinctly in reverse order
+        //8) Create a method to check if the lengths of all elements are less than 12
+        //9) Create a method to check if the initial of any element is not ‘X’
+        //10) Create a method to check if at least one of the elements ending with "R"
+
+
+        List<Integer> l4 = new ArrayList<Integer>();
+        l4.add(12);
+        l4.add(9);
+        l4.add(13);
+        l4.add(4);
+        l4.add(9);
+        l4.add(2);
+        l4.add(4);
+        l4.add(12);
+        l4.add(15);
+
+        //1) Find the sum of all elements in the list
+        //2) Find the sum of integers from 7 to 100
+        //3) Find the multiplication of the integers from 2(inc) to 11(inc)
+        //4) Calculate the factorial of a given number.(5 factorial = 1*2*3*4*5  ==> 5! = 1*2*3*4*5)
+        //5) Calculate the sum of even integers between given two integers
+        //6)Create a method to calculate the sum of digits of every integers between given two integers
+
+
+
+    }
+}
